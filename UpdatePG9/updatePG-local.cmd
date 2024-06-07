8683b0a7eb93f8a774f41a3d3adef1be9c7d57e8
DevOps Test <kukushkina@nsgc.ru>
2024-06-07 13:31:38 +0300

    UpdatePG9

---
+++ UpdatePG9/updatePG-local.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -noninteractive -windowStyle hidden -command -ExecutionPolicy ByPass C:\UpdatePG9\updatePG-local.ps1
---
+++ UpdatePG9/updatePG-shop.bat
@@ -0,0 +1,3 @@
+@echo off
+
+%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass C:\UpdatePG9\updatePG-shop.ps1
